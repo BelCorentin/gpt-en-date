1b239bbc542e8da5722ed7176746c6e6285543ce
co <corentin.bel@protonmail.com>
2023-04-24 00:17:31 +0200

    added difficulty, but broke the chat. Has to be fixed

--- app.py
+++ app.py
@@ -4,22 +4,41 @@ import openai
 app = Flask(__name__)
 
 
-conversation = [
-    {
-        "role": "system",
-        "content": "You are a women that recently just matched a guy on Tinder. Engage in a flirty conversation. If the person you're talking to doesn't seem like he would be a good date, just say \"I'm blocking you bye\"",
-    }
-]
-
-
 @app.route("/")
 def index():
-    return render_template("chat.html")
+    return render_template("welcome.html")
+
+
+@app.route("/chat", methods=["GET", "POST"])
+def go_chat():
+    if request.method == "POST":
+        difficulty = request.form.get("difficulty")
+        return render_template("chat.html", difficulty=difficulty)
+    else:
+        return render_template("chat.html")
 
 
 @app.route("/get-response", methods=["POST"])
 def get_response():
     prompt = request.json["text"]
+
+    difficulty = request.json["difficulty"]  # Get from form submission
+    if difficulty == "easy":
+        prompt_diff = "You are an easygoing, flirty woman looking to chat..."
+    elif difficulty == "medium":
+        prompt_diff = "You are a casual woman open to friendly conversation... Not sure of what she's looking for on Tinder"
+    elif difficulty == "hard":
+        prompt_diff = "You are an egocentric woman, that needs to be praised a lot to accept dates. You are bipolar."
+    elif difficulty == "really-hard":
+        prompt_diff = "You got lost on Tinder, and installed it to make friends. You're really hard to flirt with, defensive, and hard to convince to go on a date with."
+
+    conversation = [
+        {
+            "role": "system",
+            "content": f"You are a women that recently just matched a guy on Tinder. {prompt_diff} If the person you're talking to doesn't seem like he would be a good date, just say \"I am not interested, sorry. Bye :-)\"",
+        }
+    ]
+
     conversation.append({"role": "user", "content": prompt})
 
     response = openai.ChatCompletion.create(
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const difficultyForm = document.getElementById('difficulty-form');
     const messageForm = document.getElementById('message-form');
     const messageInput = document.getElementById('message-input');
     const chatMessages = document.getElementById('chat-messages');
+    const header = document.getElementById('chat-header');
+    const difficulty = document.getElementById('difficulty');
 
+    difficultyForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const selectedDifficulty = document.querySelector('input[name="difficulty"]:checked').value;
+        window.location.href = `/chat?difficulty=${selectedDifficulty}`;
+    });
     messageForm.addEventListener('submit', async (e) => {
         e.preventDefault();
         const messageText = messageInput.value.trim();
@@ -21,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ text: messageText }),
+            body: JSON.stringify({ text: messageText, difficulty: diff }),
         });
 
         const data = await response.json();
@@ -38,4 +46,46 @@ document.addEventListener('DOMContentLoaded', () => {
         chatMessages.appendChild(messageEl);
         chatMessages.scrollTop = chatMessages.scrollHeight;
     }
+
+    // function appendMessage(text, sender) {
+    //     const messageEl = document.createElement('div');
+    //     messageEl.className = `message ${sender}`;
+
+    //     const imgEl = document.createElement('img');
+    //     imgEl.src = sender === 'user' ? 'static/images/user.png' : 'static/images/bot.png';
+    //     imgEl.alt = sender === 'user' ? 'User' : 'Bot';
+    //     messageEl.appendChild(imgEl);
+
+    //     const textEl = document.createElement('div');
+    //     textEl.className = 'text';
+    //     textEl.textContent = text;
+    //     messageEl.appendChild(textEl);
+
+    //     chatMessages.appendChild(messageEl);
+    //     chatMessages.scrollTop = chatMessages.scrollHeight;
+    // }
+
+    function winAnimation() {
+        document.querySelector('.chat-container').classList.add('win-animation');
+        setTimeout(() => {
+            document.querySelector('.chat-container').classList.remove('win-animation');
+        }, 2000);  // Animation duration
+    }
+
+    function loseAnimation() {
+        document.querySelector('.chat-container').classList.add('lose-animation');
+        setTimeout(() => {
+            document.querySelector('.chat-container').classList.remove('lose-animation');
+        }, 2000);
+    }
+
+    function addDifficulty(difficulty) {
+        const difficulty_ = document.createElement('span');
+        difficulty_.className = 'difficulty-set';
+        difficulty_.textContent = difficulty;
+        header.appendChild(difficulty_);
+    }
+
+    addDifficulty(difficulty.textContent);
 });
+
